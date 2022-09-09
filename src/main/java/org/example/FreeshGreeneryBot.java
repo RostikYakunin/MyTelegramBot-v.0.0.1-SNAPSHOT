6eecfcd6b5ed5f2801b2d08d1dc7e33cf33ecde5
Rostyslav Yakunin <rostishka1310942@gmail.com>
2022-09-09 22:47:19 +0300

    create: created new class with base functional

---
+++ src/main/java/org/example/FreeshGreeneryBot.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class FreeshGreeneryBot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new FreeshGreeneryBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return "FreeshGreeneryBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5766444296:AAEYwZdLAqQDBglMP2PIbxi5LIOoUep8qfU";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
